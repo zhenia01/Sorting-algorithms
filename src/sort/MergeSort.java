56d8d0fa751daacc7b3d47adcfad87a7df9122db
Zhenia Borodaikevych <borodaykevich@gmail.com>
2019-02-16 16:13:31 +0200

    optimized

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -6,110 +6,114 @@ public class MergeSort implements SortStrategy {
 
     @Override
     public void sortDec(Comparable[] arr) {
-        Comparable[] aux = new Comparable[arr.length];
-        mergeSortDec(arr, aux, 0, arr.length - 1);
+        Comparable[] temp = new Comparable[arr.length];
+        mergeSortDec(arr, temp, 0, arr.length - 1);
     }
 
     @Override
     public void sortInc(Comparable[] arr) {
-        Comparable[] aux = new Comparable[arr.length];
-        mergeSortInc(arr, aux, 0, arr.length - 1);
+        Comparable[] temp = new Comparable[arr.length];
+        mergeSortInc(arr, temp, 0, arr.length - 1);
     }
 
     @Override
     public void sortDecComp(Object[] arr, Comparator comp) {
-        Object[] aux = new Object[arr.length];
-        mergeSortDecComp(arr, aux, 0, arr.length - 1, comp);
+        Object[] temp = new Object[arr.length];
+        mergeSortDecComp(arr, temp, 0, arr.length - 1, comp);
     }
 
     @Override
     public void sortIncComp(Object[] arr, Comparator comp) {
-        Object[] aux = new Object[arr.length];
-        mergeSortIncComp(arr, aux, 0, arr.length - 1, comp);
+        Object[] temp = new Object[arr.length];
+        mergeSortIncComp(arr, temp, 0, arr.length - 1, comp);
     }
 
-    private void mergeInc(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
+    private void mergeInc(Comparable[] a, Comparable[] temp, int begin, int mid, int end) {
+        for (int k = begin; k <= end; k++) {
+            temp[k] = a[k];
         }
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) a[k] = aux[j++];
-            else if (j > hi) a[k] = aux[i++];
-            else if (aux[j].compareTo(aux[i]) < 0) a[k] = aux[j++];
-            else a[k] = aux[i++];
-        }
-    }
-
-    private void mergeSortInc(Comparable[] a, Comparable[] aux, int lo, int hi) {
-        if (hi <= lo) return;
-        int mid = lo + (hi - lo) / 2;
-        mergeSortInc(a, aux, lo, mid);
-        mergeSortInc(a, aux, mid + 1, hi);
-        mergeInc(a, aux, lo, mid, hi);
-    }
-
-    private void mergeDec(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
-        }
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) a[k] = aux[j++];
-            else if (j > hi) a[k] = aux[i++];
-            else if (aux[j].compareTo(aux[i]) > 0) a[k] = aux[j++];
-            else a[k] = aux[i++];
+        int i = begin, j = mid + 1;
+        for (int k = begin; k <= end; k++) {
+            if (i > mid) a[k] = temp[j++];
+            else if (j > end) a[k] = temp[i++];
+            else if (temp[j].compareTo(temp[i]) < 0) a[k] = temp[j++];
+            else a[k] = temp[i++];
         }
     }
 
-    private void mergeSortDec(Comparable[] a, Comparable[] aux, int lo, int hi) {
-        if (hi <= lo) return;
-        int mid = lo + (hi - lo) / 2;
-        mergeSortDec(a, aux, lo, mid);
-        mergeSortDec(a, aux, mid + 1, hi);
-        mergeDec(a, aux, lo, mid, hi);
+    private void mergeSortInc(Comparable[] a, Comparable[] temp, int begin, int end) {
+        if (end <= begin) return;
+        int mid = begin + (end - begin) / 2;
+        mergeSortInc(a, temp, begin, mid);
+        mergeSortInc(a, temp, mid + 1, end);
+        if (a[mid].compareTo(a[mid+1]) <= 0) return;
+        mergeInc(a, temp, begin, mid, end);
     }
 
-    private void mergeIncComp(Object[] a, Object[] aux, int lo, int mid, int hi, Comparator comp) {
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
+    private void mergeDec(Comparable[] a, Comparable[] temp, int begin, int mid, int end) {
+        for (int k = begin; k <= end; k++) {
+            temp[k] = a[k];
         }
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) a[k] = aux[j++];
-            else if (j > hi) a[k] = aux[i++];
-            else if (comp.compare(aux[j], aux[i]) < 0) a[k] = aux[j++];
-            else a[k] = aux[i++];
-        }
-    }
-
-    private void mergeSortIncComp(Object[] a, Object[] aux, int lo, int hi, Comparator comp) {
-        if (hi <= lo) return;
-        int mid = lo + (hi - lo) / 2;
-        mergeSortIncComp(a, aux, lo, mid, comp);
-        mergeSortIncComp(a, aux, mid + 1, hi, comp);
-        mergeIncComp(a, aux, lo, mid, hi, comp);
-    }
-
-    private void mergeDecComp(Object[] a, Object[] aux, int lo, int mid, int hi, Comparator comp) {
-        for (int k = lo; k <= hi; k++) {
-            aux[k] = a[k];
-        }
-        int i = lo, j = mid + 1;
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid) a[k] = aux[j++];
-            else if (j > hi) a[k] = aux[i++];
-            else if (comp.compare(aux[j], aux[i]) > 0) a[k] = aux[j++];
-            else a[k] = aux[i++];
+        int i = begin, j = mid + 1;
+        for (int k = begin; k <= end; k++) {
+            if (i > mid) a[k] = temp[j++];
+            else if (j > end) a[k] = temp[i++];
+            else if (temp[j].compareTo(temp[i]) > 0) a[k] = temp[j++];
+            else a[k] = temp[i++];
         }
     }
 
-    private void mergeSortDecComp(Object[] a, Object[] aux, int lo, int hi, Comparator comp) {
-        if (hi <= lo) return;
-        int mid = lo + (hi - lo) / 2;
-        mergeSortDecComp(a, aux, lo, mid, comp);
-        mergeSortDecComp(a, aux, mid + 1, hi, comp);
-        mergeDecComp(a, aux, lo, mid, hi, comp);
+    private void mergeSortDec(Comparable[] a, Comparable[] temp, int begin, int end) {
+        if (end <= begin) return;
+        int mid = begin + (end - begin) / 2;
+        mergeSortDec(a, temp, begin, mid);
+        mergeSortDec(a, temp, mid + 1, end);
+        if (a[mid].compareTo(a[mid+1]) >= 0) return;
+        mergeDec(a, temp, begin, mid, end);
+    }
+
+    private void mergeIncComp(Object[] a, Object[] temp, int begin, int mid, int end, Comparator comp) {
+        for (int k = begin; k <= end; k++) {
+            temp[k] = a[k];
+        }
+        int i = begin, j = mid + 1;
+        for (int k = begin; k <= end; k++) {
+            if (i > mid) a[k] = temp[j++];
+            else if (j > end) a[k] = temp[i++];
+            else if (comp.compare(temp[j], temp[i]) < 0) a[k] = temp[j++];
+            else a[k] = temp[i++];
+        }
+    }
+
+    private void mergeSortIncComp(Object[] a, Object[] temp, int begin, int end, Comparator comp) {
+        if (end <= begin) return;
+        int mid = begin + (end - begin) / 2;
+        mergeSortIncComp(a, temp, begin, mid, comp);
+        mergeSortIncComp(a, temp, mid + 1, end, comp);
+        if (comp.compare(a[mid], a[mid+1]) <= 0) return;
+        mergeIncComp(a, temp, begin, mid, end, comp);
+    }
+
+    private void mergeDecComp(Object[] a, Object[] temp, int begin, int mid, int end, Comparator comp) {
+        for (int k = begin; k <= end; k++) {
+            temp[k] = a[k];
+        }
+        int i = begin, j = mid + 1;
+        for (int k = begin; k <= end; k++) {
+            if (i > mid) a[k] = temp[j++];
+            else if (j > end) a[k] = temp[i++];
+            else if (comp.compare(temp[j], temp[i]) > 0) a[k] = temp[j++];
+            else a[k] = temp[i++];
+        }
+    }
+
+    private void mergeSortDecComp(Object[] a, Object[] temp, int begin, int end, Comparator comp) {
+        if (end <= begin) return;
+        int mid = begin + (end - begin) / 2;
+        mergeSortDecComp(a, temp, begin, mid, comp);
+        mergeSortDecComp(a, temp, mid + 1, end, comp);
+        if (comp.compare(a[mid], a[mid+1]) >= 0) return;
+        mergeDecComp(a, temp, begin, mid, end, comp);
     }
 
 }
--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -28,150 +28,156 @@ public class QuickSort implements SortStrategy {
 
         if (end - begin <= 10) {
             insertionSortInc(arr, comp, begin, end);
-
-            Object mid = arr[(end + begin) / 2]; //element, not index!
-            int i = begin;
-            int j = end;
-
-            do {
-                while (comp.compare(arr[i], mid) < 0) {
-                    i++;
-                }
-
-                while (comp.compare(arr[j], mid) > 0) {
-                    j--;
-                }
-
-                if (i <= j) {
-                    Object temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    i++;
-                    j--;
-                }
-            } while (i <= j);
-
-            if (i < end) {
-                quickSortInc(arr, i, end, comp);
-            }
-
-            if (j > begin) {
-                quickSortInc(arr, begin, j, comp);
-            }
+            return;
         }
+
+        Object mid = arr[(end + begin) / 2]; //element, not index!
+        int i = begin;
+        int j = end;
+
+        do {
+            while (comp.compare(arr[i], mid) < 0) {
+                i++;
+            }
+
+            while (comp.compare(arr[j], mid) > 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                Object temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        } while (i <= j);
+
+        if (i < end) {
+            quickSortInc(arr, i, end, comp);
+        }
+
+        if (j > begin) {
+            quickSortInc(arr, begin, j, comp);
+        }
+
     }
 
     private void quickSortDec(Object[] arr, int begin, int end, Comparator comp) {
 
         if (end - begin <= 10) {
             insertionSortDec(arr, comp, begin, end);
-
-            Object mid = arr[(end + begin) / 2]; //element, not index!
-            int i = begin;
-            int j = end;
-
-            do {
-                while (comp.compare(arr[i], mid) > 0) {
-                    i++;
-                }
-
-                while (comp.compare(arr[j], mid) < 0) {
-                    j--;
-                }
-
-                if (i <= j) {
-                    Object temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    i++;
-                    j--;
-                }
-            } while (i <= j);
-
-            if (i < end) {
-                quickSortDec(arr, i, end, comp);
-            }
-
-            if (j > begin) {
-                quickSortDec(arr, begin, j, comp);
-            }
+            return;
         }
+
+        Object mid = arr[(end + begin) / 2]; //element, not index!
+        int i = begin;
+        int j = end;
+
+        do {
+            while (comp.compare(arr[i], mid) > 0) {
+                i++;
+            }
+
+            while (comp.compare(arr[j], mid) < 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                Object temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        } while (i <= j);
+
+        if (i < end) {
+            quickSortDec(arr, i, end, comp);
+        }
+
+        if (j > begin) {
+            quickSortDec(arr, begin, j, comp);
+        }
+
     }
 
     private void quickSortInc(Comparable[] arr, int begin, int end) {
 
         if (end - begin <= 10) {
             insertionSortInc(arr, begin, end);
-        } else {
-
-            Comparable mid = arr[(end + begin) / 2]; //element, not index!
-            int i = begin;
-            int j = end;
-
-            do {
-                while (arr[i].compareTo(mid) < 0) {
-                    i++;
-                }
-
-                while (arr[j].compareTo(mid) > 0) {
-                    j--;
-                }
-
-                if (i <= j) {
-                    Comparable temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    i++;
-                    j--;
-                }
-            } while (i <= j);
-
-            if (i < end) {
-                quickSortInc(arr, i, end);
-            }
-
-            if (j > begin) {
-                quickSortInc(arr, begin, j);
-            }
+            return;
         }
+
+        Comparable mid = arr[(end + begin) / 2]; //element, not index!
+        int i = begin;
+        int j = end;
+
+        do {
+            while (arr[i].compareTo(mid) < 0) {
+                i++;
+            }
+
+            while (arr[j].compareTo(mid) > 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                Comparable temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        } while (i <= j);
+
+        if (i < end) {
+            quickSortInc(arr, i, end);
+        }
+
+        if (j > begin) {
+            quickSortInc(arr, begin, j);
+        }
+
     }
 
     private void quickSortDec(Comparable[] arr, int begin, int end) {
 
         if (end - begin <= 10) {
             insertionSortDec(arr, begin, end);
-        } else {
-
-            Comparable mid = arr[(end + begin) / 2]; //element, not index!
-            int i = begin;
-            int j = end;
-
-            do {
-                while (arr[i].compareTo(mid) > 0) {
-                    i++;
-                }
-
-                while (arr[j].compareTo(mid) < 0) {
-                    j--;
-                }
-
-                if (i <= j) {
-                    Comparable temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    i++;
-                    j--;
-                }
-            } while (i <= j);
-
-            if (i < end) {
-                quickSortDec(arr, i, end);
-            }
-
-            if (j > begin) {
-                quickSortDec(arr, begin, j);
-            }
+            return;
         }
+
+        Comparable mid = arr[(end + begin) / 2]; //element, not index!
+        int i = begin;
+        int j = end;
+
+        do {
+            while (arr[i].compareTo(mid) > 0) {
+                i++;
+            }
+
+            while (arr[j].compareTo(mid) < 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                Comparable temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        } while (i <= j);
+
+        if (i < end) {
+            quickSortDec(arr, i, end);
+        }
+
+        if (j > begin) {
+            quickSortDec(arr, begin, j);
+        }
+
     }
 
     private void insertionSortInc(Comparable[] arr, int begin, int end) {
@@ -187,6 +193,7 @@ public class QuickSort implements SortStrategy {
             }
         }
     }
+
     private void insertionSortDec(Comparable[] arr, int begin, int end) {
         for (int i = begin; i <= end; i++) {
             int j = i;
